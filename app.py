7dedb1abed2b200d445ab127f9a01ecf96f36ef6
Shruthi1392 <shruthisrinivas1392@gmail.com>
2025-08-21 00:01:05 +0530

    update and deltion of tasks
    interactive rebase in progress; onto a3f15bb
    Last command done (1 command done):
       pick a7b6e5a update and deltion of tasks
    No commands remaining.
    You are currently rebasing branch 'master' on 'a3f15bb'.
      (fix conflicts and then run "git rebase --continue")
      (use "git rebase --skip" to skip this patch)
      (use "git rebase --abort" to check out the original branch)
    
    Unmerged paths:
      (use "git restore --staged <file>..." to unstage)
      (use "git add <file>..." to mark resolution)
            both added:      app.py
    
    no changes added to commit (use "git add" and/or "git commit -a")#    pick a7b6e5a update and deltion of tasks

--- app.py
+++ app.py
@@ -15,5 +15,19 @@ def add_tasks():
     tasks.append(task)
     return jsonify(task), 201
 
+@app.route("/tasks/<int:task_id>", methods=["PUT"])
+def update_task(task_id):
+    for task in tasks:
+        if tasks["id"]==task_id:
+            tasks["done"]=True
+            return jsonify(task)
+        return jsonify({"error":"Task not found"}),404
+    
+@app.route("/tasks/<int:task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    global tasks
+    tasks=[task for task in tasks if tasks["id"]!=task_id]
+    return jsonify({"message":"Task deleted."})
+
 if __name__=="__main__":
     app.run(debug=True)
